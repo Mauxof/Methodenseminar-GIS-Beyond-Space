f10d8e8222f93e5523bc4bd23e6ebac4fe28a724
Mauxof <78745822+Mauxof@users.noreply.github.com>
2022-03-09 16:07:51 +0100

    Delete Pie Chart.py

--- Pie Chart.py
+++
@@ -1,7 +0,0 @@
-import plotly.express as px
-import pandas as pd
-
-df = pd.read_csv('sz_fig.csv')
-fig = px.pie(df, values='loc_frequency', names='fcode', title='Räumliche Ebenen SZ')
-fig.show()
-fig.write_html("sz_piechart.html")
